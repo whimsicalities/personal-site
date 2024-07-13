a4da4daf2277b33d1525c0c6cee7a893e19754ce
whimsicalities <whimsicalities.code@gmail.com>
2024-07-13 12:48:58 +0100

    Add image of Legs

--- client/whimsicalities/src/app/app.component.ts
+++ client/whimsicalities/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HealthcheckComponent } from './healthcheck/healthcheck.component';
+import { PetComponent } from './pet/pet.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HealthcheckComponent],
+  imports: [RouterOutlet, HealthcheckComponent, PetComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ client/whimsicalities/src/app/pet/pet.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pet',
+  standalone: true,
+  imports: [],
+  templateUrl: './pet.component.html',
+  styleUrl: './pet.component.scss'
+})
+export class PetComponent {
+
+}
