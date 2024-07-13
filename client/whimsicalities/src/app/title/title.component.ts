2e3eced02bd3fa6b08aeee00aab6cfbe1d967ae8
whimsicalities <whimsicalities.code@gmail.com>
2024-07-13 16:58:17 +0100

    Title bar

--- client/whimsicalities/src/app/title/title.component.ts
+++ client/whimsicalities/src/app/title/title.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faMastodon } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-title',
   standalone: true,
-  imports: [],
+  imports: [ FontAwesomeModule ],
   templateUrl: './title.component.html',
   styleUrl: './title.component.scss'
 })
 export class TitleComponent {
-
+  faMastodon = faMastodon;
+  faGithub = faGithub;
 }
