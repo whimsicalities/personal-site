9f7ee7f7e277f77a904af9bb624b687e2870f945
whimsicalities <whimsicalities.code@gmail.com>
2024-07-13 14:23:21 +0100

    Miracode title

--- client/whimsicalities/src/app/app.component.ts
+++ client/whimsicalities/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HealthcheckComponent } from './healthcheck/healthcheck.component';
 import { PetComponent } from './pet/pet.component';
+import { TitleComponent } from './title/title.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HealthcheckComponent, PetComponent],
+  imports: [RouterOutlet, HealthcheckComponent, PetComponent, TitleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
