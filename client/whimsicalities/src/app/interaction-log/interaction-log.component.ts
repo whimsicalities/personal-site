af37dbbbc53550a4563732f2c78c59d09efa1dad
whimsicalities <whimsicalities.code@gmail.com>
2024-07-14 18:04:12 +0100

    Empty placeholder components for pet interaction

---
+++ client/whimsicalities/src/app/interaction-log/interaction-log.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interaction-log',
+  standalone: true,
+  imports: [],
+  templateUrl: './interaction-log.component.html',
+  styleUrl: './interaction-log.component.scss'
+})
+export class InteractionLogComponent {
+
+}
---
+++ client/whimsicalities/src/app/pet-interaction-interface/pet-interaction-interface.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-interaction-interface',
+  standalone: true,
+  imports: [],
+  templateUrl: './pet-interaction-interface.component.html',
+  styleUrl: './pet-interaction-interface.component.scss'
+})
+export class PetInteractionInterfaceComponent {
+
+}
