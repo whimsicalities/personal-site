553b64f505167c6a09f5ff220363c930b1490865
whimsicalities <whimsicalities.code@gmail.com>
2024-07-14 18:03:43 +0100

    Connect to disposeable redis container

--- server/whimsicalities-server/src/server.ts
+++ server/whimsicalities-server/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import configs from './config/configs';
 import EnvironmentConfig from './config/EnvironmentConfig';
+import { createClient } from 'redis';
 
 let config: EnvironmentConfig;
 
@@ -25,10 +26,25 @@ const corsOptions = {
   origin: config.corsOrigin,
 }
 
+const connectToRedis = async () => {
+  const client = createClient({
+    url: 'redis://@redis:6379'
+  });
+
+  client.on('error', err => console.log('Redis Client Error', err));
+
+  await client.connect();
+
+  await client.set('key', 'value');
+  const value = await client.get('key');
+  console.log(value);
+}
+
 app.get('/healthcheck', cors(corsOptions), (req, res) => {
   res.send(true);
 })
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
+  console.log(`Server listening on port ${port}`);
+  connectToRedis();
 })
