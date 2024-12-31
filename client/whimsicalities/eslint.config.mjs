aa4e155d09c7417351d5f1d33a67573e4aba93e5
whimsicalities <whimsicalities.code@gmail.com>
2024-12-31 11:19:56 +0000

    Apply semi rule for eslint

---
+++ client/whimsicalities/eslint.config.mjs
@@ -0,0 +1,39 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import angular from 'angular-eslint';
+import stylistic from '@stylistic/eslint-plugin'
+
+export default tseslint.config({
+  files: ["**/*.ts"],
+  plugins: {
+    '@stylistic': stylistic
+  },
+  extends: [
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...tseslint.configs.stylistic,
+    ...angular.configs.tsRecommended,
+  ],
+  processor: angular.processInlineTemplates,
+  rules: {
+    "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+    "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
+    "@stylistic/semi": ["warn"],
+  },
+});
--- server/whimsicalities-server/eslint.config.mjs
+++ server/whimsicalities-server/eslint.config.mjs
@@ -2,14 +2,19 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin'
 
 export default tseslint.config({
   files: ["**/*.ts"],
+  plugins: {
+    '@stylistic': stylistic
+  },
   extends: [
     eslint.configs.recommended,
     tseslint.configs.recommended,
   ],
   rules: {
-    "@typescript-eslint/no-explicit-any": 'off'
+    "@typescript-eslint/no-explicit-any": 'off',
+    "@stylistic/semi": ["warn"],
   }
 });
